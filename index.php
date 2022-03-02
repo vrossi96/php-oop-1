10b183159fac89e557e22d5ab9adb9a253e7a466
vrossi96 <vrossi396@yahoo.it>
2022-03-02 13:49:38 +0100

    add class and construct

--- index.php
+++ index.php
@@ -1,11 +1,31 @@
 <?php
 /* Oggi pomeriggio ripassate i primi concetti di classe e di variabili e
 metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui: */
-// - è definita una classe ‘Movie’
-//    => all'interno della classe sono dichiarate delle variabili d'istanza
-//    => all'interno della classe è definito un costruttore
+//| - è definita una classe ‘Movie’
+//|    => all'interno della classe sono dichiarate delle variabili d'istanza
+//|    => all'interno della classe è definito un costruttore
 //    => all'interno della classe è definito almeno un metodo
 // - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
+
+class Movies
+{
+
+   public $title;
+   public $year;
+   public $poster;
+   public $protagonist;
+   public $actor;
+
+   public function __construct($title, $year, $poster, $protagonist, $actor)
+   {
+      $this->title = $title;
+      $this->year = $year;
+      $this->poster = $poster;
+      $this->protagonist = $protagonist;
+      $this->actor = $actor;
+   }
+}
+
 ?>
 
 <!DOCTYPE html>
