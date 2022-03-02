f6108e52aa68f92599e339519822587d57243231
vrossi96 <vrossi396@yahoo.it>
2022-03-02 13:42:08 +0100

    init

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/* Oggi pomeriggio ripassate i primi concetti di classe e di variabili e
+metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui: */
+// - è definita una classe ‘Movie’
+//    => all'interno della classe sono dichiarate delle variabili d'istanza
+//    => all'interno della classe è definito un costruttore
+//    => all'interno della classe è definito almeno un metodo
+// - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>PHP-OOP-1</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
