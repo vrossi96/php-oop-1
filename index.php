f90dd1daae12804bc5f907dd72a3e1d29399b5d7
vrossi96 <vrossi396@yahoo.it>
2022-03-02 14:07:10 +0100

    add two movies

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
 //| - è definita una classe ‘Movie’
 //|    => all'interno della classe sono dichiarate delle variabili d'istanza
 //|    => all'interno della classe è definito un costruttore
-//    => all'interno della classe è definito almeno un metodo
+//|    => all'interno della classe è definito almeno un metodo
 // - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
 
 class Movies
@@ -15,17 +15,32 @@ class Movies
    public $poster;
    public $protagonist;
    public $actor;
+   public $rating;
+   public $stars;
 
-   public function __construct($title, $year, $poster, $protagonist, $actor)
+   public function __construct($title, $year, $poster, $protagonist, $actor, $rating)
    {
       $this->title = $title;
       $this->year = $year;
       $this->poster = $poster;
       $this->protagonist = $protagonist;
       $this->actor = $actor;
+      $this->rating = $rating;
+      $this->stars = $this->getStars();
+   }
+
+   public function getStars()
+   {
+      return $this->rating / 20;
    }
 }
 
+$movie1 = new Movies('American Psycho', 2000, 'https://m.media-amazon.com/images/M/MV5BZTM2ZGJmNjQtN2UyOS00NjcxLWFjMDktMDE2NzMyNTZlZTBiXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg', 'Patrick Bateman', 'Christian Bale', 85);
+
+$movie2 = new Movies('Drive', 2011, 'https://m.media-amazon.com/images/I/81tdQqbCdWL._AC_SL1500_.jpg', 'The pilot', 'Ryan Gosling', 79);
+
+var_dump($movie1);
+var_dump($movie2);
 ?>
 
 <!DOCTYPE html>
